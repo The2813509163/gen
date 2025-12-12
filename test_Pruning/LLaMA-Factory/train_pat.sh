445683cb1cb556b53db1ad351e1d84302bc38128
The2813509163 <2813509163@qq.com>
2025-12-12 18:23:22 +0800

    modified:   examples/train_lora/llama3.1-8b-base_lora_sft.yaml 	modified:   train_pat.sh

--- test_Pruning/LLaMA-Factory/train_pat.sh
+++ test_Pruning/LLaMA-Factory/train_pat.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-export CUDA_VISIBLE_DEVICES=2,3,4,5,6,7
-export MODELPATH="/data/home/duyuanProfV2/workspace/qianxuzhen/data/kris/shared_data/models/Llama-3.2-3B"
-llamafactory-cli train examples/train_lora/test.yaml
-
+export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
+export TRAINER="Super2Trainer"
+llamafactory-cli train examples/train_lora/llama3.1-8b-base_lora_sft.yaml
 
