382bf5416ceeb9f08d60536fa9a54eb13f201b89
The2813509163 <2813509163@qq.com>
2025-12-14 19:52:07 +0800

    modified:   train_pat.sh

--- test_Pruning/LLaMA-Factory/train_pat.sh
+++ test_Pruning/LLaMA-Factory/train_pat.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
 export TRAINER="Super2Trainer"
+export WANDB_PROJECT="Llama3-Experiment" 
+export WANDB_RUN_NAME="1214-test-pruning"
 llamafactory-cli train examples/train_lora/llama3.1-8b-base_lora_sft.yaml
 
