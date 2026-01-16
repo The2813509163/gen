8a472b1f76091cded1ea1ac165b60dabc31babd1
2024chunji <221870018@smail.nju.edu.cn>
2026-01-16 17:08:16 +0800

    modified:   run_evaluation.sh 	new file:   train_and_eval.sh

--- test_Pruning/LLaMA-Factory/run_evaluation.sh
+++ test_Pruning/LLaMA-Factory/run_evaluation.sh
@@ -2,9 +2,9 @@
 
 # 设置参数
 HF_TYPE="base"
-HF_PATH="/data/home/duyuanProfV2/workspace/qianxuzhen/data/kris/shared_data/models/Llama-3.2-3B"
-PEFT_PATH="/data/home/duyuanProfV2/workspace/qianxuzhen/data/kris/qianxuzhen/Pruning-LLMs/LLaMA-Factory/saves/meta-llama__Llama-3.2-3B-tap0.9-learnable-interm/lora/sft/max_samples_gen_dummy_1M_alpha_dynamic_0.0001"
-DATASETS="gsm8k_gen_1d7fe4 math_4shot_base_gen_db136b svamp_gen_fb25e4 piqa_gen_1194eb siqa_gen_18632c squad20_gen_1710bc ARC_c_gen_1e0de5 ARC_e_gen_1e0de5 lambada_gen_217e11"
+HF_PATH="meta-llama/Llama-3.1-8B"
+PEFT_PATH="/lustre/fsw/portfolios/edgeai/users/yilzhao/CODES/gen/tmp/outputs/main/0107-1"
+DATASETS="gsm8k_gen_1d7fe4 math_4shot_base_gen_db136b svamp_gen_fb25e4 piqa_gen_1194eb siqa_gen_18632c squad20_gen_1710bc ARC_c_gen_1e0de5 ARC_e_gen_1e0de5 lambada_gen_217e11_mmlu_ppl"
 BATCH_SIZE=16
 MAX_OUT_LEN=512
 MAX_NUM_WORKERS=8
@@ -13,7 +13,7 @@ CUDA_VISIBLE_DEVICES="0,1,2,3,4,5,6,7"
 # 设置环境变量
 export CUDA_VISIBLE_DEVICES=$CUDA_VISIBLE_DEVICES
 export update_step=78111
-export tap_args='{"tap_enabled": true,"tap_stop_at_steps": 8000,"tap_remain_ratio": 0.9}'
+export tap_args='{"tap_enabled": true,"tap_stop_at_steps": 16000,"tap_remain_ratio": 0.9}'
 export learnable_mask=true
 export HIO_r=512
 
