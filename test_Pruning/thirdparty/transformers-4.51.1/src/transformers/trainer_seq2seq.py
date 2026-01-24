a179524a4e6883f239b95a8407562e8ffe80eb07
2024chunji <221870018@smail.nju.edu.cn>
2026-01-24 15:25:06 +0800

    modified:   trainer_seq2seq.py

--- test_Pruning/thirdparty/transformers-4.51.1/src/transformers/trainer_seq2seq.py
+++ test_Pruning/thirdparty/transformers-4.51.1/src/transformers/trainer_seq2seq.py
@@ -27,7 +27,7 @@ from .generation.configuration_utils import GenerationConfig
 from .integrations.deepspeed import is_deepspeed_zero3_enabled
 from .integrations.fsdp import is_fsdp_managed_module
 from .trainer import Trainer
-from .trainer import CustomTrainer,DummyTrainer,SuperTrainer,Super2Trainer,PruningTrainer # my_modify
+from .trainer import CustomTrainer,DummyTrainer,SuperTrainer,Super2Trainer,PruningTrainer,Super3Trainer # my_modify
 from .utils import is_datasets_available, logging
 from .utils.deprecation import deprecate_kwarg
 
@@ -55,6 +55,7 @@ TrainerMap = {
     "Trainer":PruningTrainer,
     "CustomTrainer":CustomTrainer,
     "Super2Trainer":Super2Trainer,
+    "Super3Trainer":Super2Trainer,
 }
 
 trainer_name = os.getenv("TRAINER")
